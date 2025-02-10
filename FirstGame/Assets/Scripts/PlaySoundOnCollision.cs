9e8a11534b68fc46c2d8c80fd39fce0bcc77700a
avokado0 <yusuf190357@outlook.com>
2025-02-10 13:11:10 +0300

    SoundResource

---
+++ FirstGame/Assets/Scripts/PlaySoundOnCollision.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class PlaySoundOnCollision : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public string targetTag = "ENGEL"; // Hedef nesnenin etiketi
+
+    private void Start()
+    {
+        if (audioSource == null)
+        {
+            audioSource = GetComponent<AudioSource>();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag(targetTag)) // Yalnýzca belirli etiketli nesneyle çarpýþýnca
+        {
+            if (audioSource != null && !audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+    }
+}
